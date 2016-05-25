5b6934fa65fc4add6b9f6287b487aece5925abc5
Todd Nine <tnine@apigee.com>
2016-05-25 15:32:06 -0600

    Finished first pass

--- apigee.go
+++ apigee.go
@@ -1,98 +1,91 @@
 package authsdk
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 //ApigeeJWTToken the apigee impelmentation of the JWT token auth sdk
 type ApigeeJWTToken struct {
-	originalToken string
-	tokenPayload  tokenPayload
-}
-
-//NewApigeeJWTTokenFromRequest create and return our JWTToken impl from the http request
-func NewApigeeJWTTokenFromRequest(r *http.Request) (JWTToken, error) {
-	header := r.Header.Get("Authorization")
-
-	if header == "" {
-		return nil, fmt.Errorf("No 'Authorization' header was found in the request")
-	}
-
-	sections := strings.Fields(header)
-
-	if len(sections) != 2 {
-		return nil, fmt.Errorf("Expected the authorization header to have the format of 'Bearer JWTTOKEN'")
-	}
-
-	//if we get here, we have a Bearer token
-
-	return NewApigeeJWTToken(sections[1])
+	tokenPayload
 }
 
 //NewApigeeJWTToken create a new Apigee JWT token.  Return the instance or an error if one cannot be created
 func NewApigeeJWTToken(token string) (JWTToken, error) {
 
-	parts := strings.Split(token, ".")
-
-	length := len(parts)
-
-	if length != 3 {
-		return nil, fmt.Errorf("Expected JWT token to contain a header, a payload, and a signature.  Only received %d parts", length)
-	}
-
-	partToDecode := parts[1]
-
-	// fmt.Printf("Decoding base64 value of %s\n", partToDecode)
-
-	decodedPayload, err := base64.RawURLEncoding.DecodeString(partToDecode)
-
-	if err != nil {
-		return nil, fmt.Errorf("Unable to decode payload.  Decode error: %s", err)
-	}
-
-	// fmt.Printf("Decoded payload is %s\n", decodedPayload)
-
-	var tokenPayload tokenPayload
-
-	err = json.Unmarshal([]byte(decodedPayload), &tokenPayload)
+	parsedToken, err := parseToken(token)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &ApigeeJWTToken{
-		originalToken: token,
-		tokenPayload:  tokenPayload,
-	}, nil
-}
-
-//GetSubject get the subject claim from the token
-func (token *ApigeeJWTToken) GetSubject() string {
-	return token.tokenPayload.Subject
-}
-
-//GetUsername return the username if possible for the subject
-func (token *ApigeeJWTToken) GetUsername() string {
-	return token.tokenPayload.Username
-}
-
-//GetEmail return the username if possible for the subject
-func (token *ApigeeJWTToken) GetEmail() string {
-	return token.tokenPayload.Email
+	return &ApigeeJWTToken{tokenPayload: *parsedToken}, nil
 }
 
 //IsOrgAdmin is the current JWTToken subject an organization admin.  If so return true, if not, return false.  An error is returned if the check cannot be performed
 func (token *ApigeeJWTToken) IsOrgAdmin(orgName string) (bool, error) {
-	//TODO, implement this
-	return true, nil
+
+	url := fmt.Sprintf("https://api.enterprise.apigee.com/v1/users/%s/userroles", token.GetUsername())
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.EncodedToken))
+	client := &http.Client{}
+	response, err := client.Do(req)
+
+	if err != nil {
+		return false, err
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return false, err
+	}
+
+	/**
+		 {
+	  "role": [
+	    {
+	      "name": "readonlyadmin",
+	      "organization": "woolworths"
+	    },
+	    {
+	      "name": "orgadmin",
+	      "organization": "michaelarusso"
+	    }
+	  ]
+	}
+		  **/
+
+	roles := &roles{}
+
+	err = json.Unmarshal(body, &roles)
+
+	if err != nil {
+		return false, err
+	}
+
+	return hasOrganizationAdmin(roles, orgName), nil
 }
 
-type tokenPayload struct {
-	Subject  string `json:"sub"`
-	Username string `json:"user_name"`
-	Email    string `json:"email"`
+//hasOrganization returns true if the organization is found
+func hasOrganizationAdmin(roles *roles, organizatioName string) bool {
+	for _, role := range roles.Role {
+		if role.Name == "orgadmin" && role.Organization == organizatioName {
+			return true
+		}
+	}
+
+	return false
+}
+
+type roles struct {
+	Role []struct {
+		Name         string `json:"name"`         // readonlyadmin
+		Organization string `json:"organization"` // woolworths
+	} `json:"role"`
 }
--- apigee_test.go
+++ apigee_test.go
@@ -3,13 +3,17 @@ package authsdk
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"os"
 )
 
 var _ = Describe("Apigee", func() {
 
 	It("Parse Token", func() {
 
-		testToken := `eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJhZGFkYjllMS02MWFmLTQ3YzYtYjdkNi00OTM3NTI4ODA3OTUiLCJzdWIiOiIzODkyNzcyNy05MzJhLTQ3MDYtYWNlYy01NmQzODIwNzdmMzQiLCJzY29wZSI6WyJzY2ltLm1lIiwib3BlbmlkIiwicGFzc3dvcmQud3JpdGUiLCJhcHByb3ZhbHMubWUiLCJvYXV0aC5hcHByb3ZhbHMiXSwiY2xpZW50X2lkIjoiZWRnZWNsaSIsImNpZCI6ImVkZ2VjbGkiLCJhenAiOiJlZGdlY2xpIiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9pZCI6IjM4OTI3NzI3LTkzMmEtNDcwNi1hY2VjLTU2ZDM4MjA3N2YzNCIsIm9yaWdpbiI6InVzZXJncmlkIiwidXNlcl9uYW1lIjoidG5pbmVAYXBpZ2VlLmNvbSIsImVtYWlsIjoidG5pbmVAYXBpZ2VlLmNvbSIsImF1dGhfdGltZSI6MTQ2NDEzMDk5MywicmV2X3NpZyI6IjZkZTJjODk2IiwiaWF0IjoxNDY0MTMwOTkzLCJleHAiOjE0NjQxMzI3OTMsImlzcyI6Imh0dHBzOi8vbG9naW4uYXBpZ2VlLmNvbS9vYXV0aC90b2tlbiIsInppZCI6InVhYSIsImF1ZCI6WyJlZGdlY2xpIiwic2NpbSIsIm9wZW5pZCIsInBhc3N3b3JkIiwiYXBwcm92YWxzIiwib2F1dGgiXX0.UF5ObmUpeqFuEv58lBYq5O59wqYRt_mb5a5Sh96n2h4DsSk_g7WQ9gupn2BTZGn2ZVyjlrXVlSR5hDTuvJ-x1dCYh3f4wZASBZwqeRqa2quYiSmrVPPXk_lUbaj379O6HOLSfkaRNyGwfGS_QaS3_odMFyoPFF3rnpa4ZpeyOY66esLHAh0JhLHtE9vBr-0q3wps7GcxOIZUe3Xne-katxX3F4e9YenADDQ7CCvZiznASdDF1mH1QQ1nNA0lCz2HPZhqLc5Axj7TGJbugHT34eNg7mltOQEgWfnXCe_IpMlgac3YHjofa8ww8imtMuqyHNE5XetJ1sCJTLNBWt_ogQ`
+		testToken := os.Getenv("TEST_APIGEE_TOKEN")
+
+		Expect(testToken).ShouldNot(BeNil(), "TEST_APIGEE_TOKEN must be defined in the environment to execute this test")
 
 		jwtToken, err := NewApigeeJWTToken(testToken)
 
@@ -22,6 +26,12 @@ var _ = Describe("Apigee", func() {
 		Expect(jwtToken.GetEmail()).Should(Equal("tnine@apigee.com"))
 
 		Expect(jwtToken.GetUsername()).Should(Equal("tnine@apigee.com"))
+
+		isAdmin, err := jwtToken.IsOrgAdmin("michaelarusso")
+
+		Expect(err).Should(BeNil(), "Should not return an error creating a valid token")
+
+		Expect(isAdmin).Should(Equal(true), "admin should be true")
 
 	})
 
