6b14ddd8c44287bebe38af3b7902d33046ac7118
Todd Nine <tnine@apigee.com>
2016-05-26 11:01:31 -0600

    Adds Caching

--- apigee.go
+++ apigee.go
@@ -10,6 +10,8 @@ import (
 //ApigeeJWTToken the apigee impelmentation of the JWT token auth sdk
 type ApigeeJWTToken struct {
 	tokenPayload
+	//the cached roles, can be nill if not set
+	roles *roles
 }
 
 //NewApigeeJWTToken create a new Apigee JWT token.  Return the instance or an error if one cannot be created
@@ -27,49 +29,55 @@ func NewApigeeJWTToken(token string) (JWTToken, error) {
 //IsOrgAdmin is the current JWTToken subject an organization admin.  If so return true, if not, return false.  An error is returned if the check cannot be performed
 func (token *ApigeeJWTToken) IsOrgAdmin(orgName string) (bool, error) {
 
-	url := fmt.Sprintf("https://api.enterprise.apigee.com/v1/users/%s/userroles", token.GetUsername())
+	//we haven't pulled the roles and cache them, go get them
+	if token.roles == nil {
 
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.EncodedToken))
-	client := &http.Client{}
-	response, err := client.Do(req)
+		url := fmt.Sprintf("https://api.enterprise.apigee.com/v1/users/%s/userroles", token.GetUsername())
 
-	if err != nil {
-		return false, err
+		req, err := http.NewRequest("GET", url, nil)
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.EncodedToken))
+		client := &http.Client{}
+		response, err := client.Do(req)
+
+		if err != nil {
+			return false, err
+		}
+
+		defer response.Body.Close()
+		body, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			return false, err
+		}
+
+		/**
+			 {
+		  "role": [
+		    {
+		      "name": "readonlyadmin",
+		      "organization": "woolworths"
+		    },
+		    {
+		      "name": "orgadmin",
+		      "organization": "michaelarusso"
+		    }
+		  ]
+		}
+			  **/
+
+		roles := &roles{}
+
+		err = json.Unmarshal(body, &roles)
+
+		if err != nil {
+			return false, err
+		}
+		token.roles = roles
+
 	}
 
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return false, err
-	}
-
-	/**
-		 {
-	  "role": [
-	    {
-	      "name": "readonlyadmin",
-	      "organization": "woolworths"
-	    },
-	    {
-	      "name": "orgadmin",
-	      "organization": "michaelarusso"
-	    }
-	  ]
-	}
-		  **/
-
-	roles := &roles{}
-
-	err = json.Unmarshal(body, &roles)
-
-	if err != nil {
-		return false, err
-	}
-
-	return hasOrganizationAdmin(roles, orgName), nil
+	return hasOrganizationAdmin(token.roles, orgName), nil
 }
 
 //hasOrganization returns true if the organization is found
